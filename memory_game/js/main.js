959418705aeb8b29cc763e5e6a049d42fa1a001c
Tan Wee Kiat <tanweekiat@Tans-MacBook-Pro.local>
2019-09-02 05:39:34 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,27 @@
-console.log("Up and running!");
 
 
-const  cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
 const cardsInPlay = [];
 function checkForMatch(){
  if (cardsInPlay[0] === cardsInPlay[1]){
@@ -12,8 +32,10 @@ function checkForMatch(){
 }
 function flipCard(cardId){
 checkForMatch();
- console.log("User flipped " + cards[cardId]);
- cardsInPlay.push(cards[cardId]);
+ console.log("User flipped " + cards[cardId].rank);
+ cardsInPlay.push(cards[cardId].rank);
+ console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
 }
 
  flipCard([0]);
