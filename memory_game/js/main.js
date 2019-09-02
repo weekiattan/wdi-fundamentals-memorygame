d97055ce7dd77f6957b7838064d915ca23de0d14
Tan Wee Kiat <tanweekiat@Tans-MacBook-Pro.local>
2019-09-02 21:02:29 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,21 +23,32 @@ var cards = [
 }
 ];
 const cardsInPlay = [];
-function checkForMatch(){
+var checkForMatch = function(){
  if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 }else {
 		alert("Sorry, try again");
 	}
 }
-function flipCard(cardId){
-checkForMatch();
+var flipCard = function(){
+var cardId = this.getAttribute('data-id')
  console.log("User flipped " + cards[cardId].rank);
  cardsInPlay.push(cards[cardId].rank);
  console.log(cards[cardId].cardImage);
  console.log(cards[cardId].suit);
+ this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+        checkForMatch();
 }
-
- flipCard([0]);
- flipCard([2]);
-	
+}
+function createBoard(){
+ for (var i = 0; i < cards.length; i++) {
+ 	var cardElement = document.createElement('img');
+ 	cardElement.setAttribute('src', 'images/back.png');
+ 	cardElement.setAttribute('data-id', i);
+ 	cardElement.addEventListener('click', flipCard);
+ 	document.getElementById('game board').appendChild(cardElement);
+    
+ }
+}
+createBoard();
